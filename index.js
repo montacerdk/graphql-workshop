f245c494db5f3b6cdfd18ea28f0e2a17d62a208a
Montacer Dkhilali <montacer.dkhilali@gmail.com>
2020-04-25 14:21:50 +0100

    Add GraphQL Middleware

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const expressGraphQL = require("express-graphql");
 
 const app = express();
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    graphiql: true,
+  })
+);
 
 app.listen(4001, () => {
   console.log("Your GraphQL server is listening on port 4001 ...");
